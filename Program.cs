df4505b1ff4d796a77d1aeeee32f94949856980f
kurodevpsps <abraaogalmeida@gmail.com>
2026-03-01 14:05:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using HttpClient client = new();
+
+try 
+{
+    string response = await client.GetStringAsync("https://api.adviceslip.com/advice");
+    using JsonDocument doc = JsonDocument.Parse(response);
+    string advice = doc.RootElement.GetProperty("slip").GetProperty("advice").GetString();
+    
+    Console.WriteLine("Conselho de Hoje:");
+    Console.WriteLine(advice);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao buscar conselho: {ex.Message}");
+}
